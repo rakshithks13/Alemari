a0a2fb48bc9f8d24b022d8a86ca2ad48da198e04
Rakshith <rakshithks13@gmail.com>
2025-05-14 14:31:29 +0530

    changes in home with alphabets

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,13 +5,13 @@ import Places from "../components/Places/Places";
 import Testimonial from "../components/Testimonial/Testimonial";
 import Banner from "../components/Banner/Banner";
 import OrderPopup from "../components/OrderPopup/OrderPopup";
-import Services from "../components/Services/services";
+import Services from "../components/Services/Services";
 import Destinations from "../components/Destininations/Destininations";
-import Subscribe from "../components/subscribe/subscribe";
+import Subscribe from "../components/subscribe/Subscribe";
 import Testimonial2 from "../components/Testimonial/Testimonial2";
-import BookingSteps from "../components/BookingSteps/bookingSteps";
-import TrendingDestinations from "../components/TrendingDestinnations/trendingDestinations";
-import Categories from "../components/Categories/categories";
+import BookingSteps from "../components/BookingSteps/BookingSteps";
+import TrendingDestinations from "../components/TrendingDestinnations/TrendingDestinations";
+import Categories from "../components/Categories/Categories";
 
 const Home = () => {
   const [orderPopup, setOrderPopup] = React.useState(false);
